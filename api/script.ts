22edc7160a867d232b95a3378a569c80b101387e
Daniel Lehmann <lehmann@taktsoft.com>
2026-01-17 08:21:48 +0100

    Improves logging

--- api/script.ts
+++ api/script.ts
@@ -4,11 +4,8 @@ BunnySDK.net.http.serve(async (request: Request): Promise<Response> => {
   const url = new URL(request.url);
   
   // Logging
-  const headers = Object.fromEntries(request.headers.entries());
-  console.log(`[DEBUG]: Headers: ${JSON.stringify(headers)}`);
-
-  const clientIp = request.headers.get("bunny-client-ip") || request.headers.get("x-forwarded-for") || "unknown";
-  const requestId = request.headers.get("cdn-request-id") || request.headers.get("x-request-id") || "unknown";
+  const clientIp = request.headers.get("x-real-ip") || request.headers.get("x-forwarded-for") || "unknown";
+  const requestId = request.headers.get("cdn-requestid") || "unknown";
   const logData = {
       timestamp: new Date().toISOString(),
       requestId: requestId,
